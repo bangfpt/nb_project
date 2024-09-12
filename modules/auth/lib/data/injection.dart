6b0df11b4376786848cb9eac94f485d6b255fdff
Nguyễn Hoàng Băng CNTT <bangnh@f88.vn>
2024-09-12 14:04:37 +0700

    rafactor: refactor module auth

--- modules/auth/lib/data/injection.dart
+++ modules/auth/lib/data/injection.dart
@@ -3,6 +3,7 @@ import 'package:dio/dio.dart';
 
 import '../domain/repository.dart';
 import 'datasource/local/shared_preference.dart';
+import 'datasource/local/shared_prefrence_imp.dart';
 import 'repository_impl.dart';
 import 'package:get_it/get_it.dart';
 
@@ -10,8 +11,13 @@ class DataInjection {
   final _getIt = GetIt.instance;
 
   Future<void> inject() async {
-    // await _injectLocalService();
+    await _injectLocalService();
     _injectRemoteService();
+  }
+
+  Future<void> _injectLocalService() async {
+    _getIt.registerSingleton<SharedPreference>(SharedPreferenceImp());
+    await _getIt.get<SharedPreference>().initialize();
   }
 
   void _injectRemoteService() {
--- modules/auth/lib/presentation/screen/auth/cubit/auth_cubit.dart
+++ modules/auth/lib/presentation/screen/auth/cubit/auth_cubit.dart
@@ -2,8 +2,8 @@ import 'package:auth/data/data.dart';
 import 'package:auth/domain/auth_controller.dart';
 import 'package:auth/domain/repository.dart';
 import 'package:auth/domain/use_case/login_use_case.dart';
-import 'package:core/core/utils/either.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:core/core/utils/either.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:get_it/get_it.dart';
 
