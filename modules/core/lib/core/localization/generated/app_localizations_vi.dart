6fea3cadb04ee08ae088910e50ea9b0003f212e9
Nguyễn Hoàng Băng CNTT <bangnh@f88.vn>
2024-09-16 10:51:38 +0700

    feat: resolve warning and problems

--- modules/core/lib/core/localization/generated/app_localizations_vi.dart
+++ modules/core/lib/core/localization/generated/app_localizations_vi.dart
@@ -1,18 +1,20 @@
 import 'app_localizations.dart';
 
+// ignore_for_file: type=lint
+
 /// The translations for Vietnamese (`vi`).
 class ModuleLocalizationImpVi extends ModuleLocalizationImp {
   ModuleLocalizationImpVi([String locale = 'vi']) : super(locale);
 
   @override
-  String get dialog_message_done_button => 'Xong';
+  String get dialogMessageDoneButton => 'Xong';
 
   @override
-  String get dialog_message_title => 'Thông báo';
+  String get dialogMessageTitle => 'Thông báo';
 
   @override
-  String get required_info => 'Thông tin bắt buộc';
+  String get requiredInfo => 'Thông tin bắt buộc';
 
   @override
-  String get invalid_input => 'Dữ liệu không hợpn lệ';
+  String get invalidInput => 'Dữ liệu không hợpn lệ';
 }
